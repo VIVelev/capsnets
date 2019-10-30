03aa5d30b200755724f8c49b3530d701f9645072
Victor Velev <velev.victor@yahoo.com>
2019-10-30 18:40:49 +0200

    [Update] Capsule dir + datasets load

--- dataset.py
+++ dataset.py
@@ -6,15 +6,28 @@ import torchvision.transforms as transforms
 __all__ = [
     'BATCH_SIZE',
     'TEST_BATCH_SIZE',
+    'NUM_WORKERS',
+
     'get_transform',
+
+    'get_train_set'
+    'get_test_set',
     'get_sets',
+
     'imshow',
+
     'get_set_loaders',
 ]
 
 
+# ====================================================================================================
+# ====================================================================================================
+
 BATCH_SIZE = 128
 TEST_BATCH_SIZE = 256
+NUM_WORKERS = 4
+
+# ====================================================================================================
 
 def get_transform():
     pil_to_tensor_transform = transforms.ToTensor()
@@ -23,28 +36,38 @@ def get_transform():
         pil_to_tensor_transform,
     ])
 
+# ====================================================================================================
+
+def get_train_set(path='./datasets', download=True):
+    return datasets.MNIST(path, train=True, download=download, transform=get_transform())
+
+def get_test_set(path='./datasets', download=True):
+    return datasets.MNIST(path, train=False, download=download, transform=get_transform())
+
 def get_sets(path='./datasets', download=True):
-    # Data transform
-    transform = get_transform()
+    return (
+        get_train_set(path, download),
+        get_test_set(path, download),
+    )
 
-    # Train set
-    train_set = datasets.MNIST(path, train=True, download=download, transform=transform)
-    # Test set
-    test_set = datasets.MNIST(path, train=False, download=download, transform=transform)
-
-    return train_set, test_set
+# ====================================================================================================
 
 def imshow(tensor):
     plt.imshow(tensor.squeeze(), cmap='binary')
     plt.show()
 
-def get_set_loaders(batch_size=BATCH_SIZE, test_batch_size=TEST_BATCH_SIZE, num_workers=4):
-    # Get sets
-    train_set, test_set = get_sets()
+# ====================================================================================================
 
-    # Train set loader
-    train_set_loader = data.DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=num_workers)
-    # Test set loader
-    test_set_loader = data.DataLoader(test_set, batch_size=test_batch_size, shuffle=False, num_workers=num_workers)
+def get_train_set_loader(batch_size=BATCH_SIZE, num_workers=NUM_WORKERS):
+    return data.DataLoader(get_train_set(), batch_size=batch_size, shuffle=True, num_workers=num_workers)
 
-    return train_set_loader, test_set_loader
+def get_test_set_loader(batch_size=TEST_BATCH_SIZE, num_workers=NUM_WORKERS):
+    return data.DataLoader(get_test_set(), batch_size=batch_size, shuffle=False, num_workers=num_workers)
+
+def get_set_loaders(batch_size=BATCH_SIZE, test_batch_size=TEST_BATCH_SIZE, num_workers=NUM_WORKERS):
+    return (
+        get_train_set_loader(batch_size, num_workers),
+        get_test_set_loader(test_batch_size, num_workers),
+    )
+
+# ====================================================================================================
